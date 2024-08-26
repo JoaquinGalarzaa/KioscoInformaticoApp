3f35f3504bf52bf6e7e44052095b1fe8ea43d497
JoaquinGalarzaa <Joaa.galarza@gmail.com>
2024-08-26 18:37:52 -0300

    Agregamos un inicio de sesion

--- KioscoInformaticoApp/App.xaml.cs
+++ KioscoInformaticoApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace KioscoInformaticoApp
+﻿using KioscoInformaticoApp.Views;
+
+namespace KioscoInformaticoApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new InicioSesionView();
         }
     }
 }
---
+++ KioscoInformaticoApp/ViewModels/InicioSesionViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KioscoInformaticoApp.Claes;
+
+namespace KioscoInformaticoApp.ViewModels
+{
+    public class InicioSesionViewModel : ObjectNotification
+    {
+		private string email;
+		public string Email
+		{
+			get { return email; }
+			set { email = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private string password;
+		public string Password
+		{
+			get { return password; }
+			set { password = value;
+                OnPropertyChanged();
+            }
+		}
+
+		private bool recordarContraseña;
+
+		public bool RecordarContraseña
+		{
+			get { return recordarContraseña; }
+			set { recordarContraseña = value;
+                OnPropertyChanged();
+            }
+		}
+
+
+	}
+}
